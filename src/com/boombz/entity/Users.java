0f7ef58fd37580f81cfad001cd69a026a8d34ea5
huiyuhang123@qq.com <huiyuhang123@qq.com>
2018-02-05 18:25:11 +0800

    Initial commit

---
+++ src/com/boombz/entity/Users.java
@@ -0,0 +1,65 @@
+package com.boombz.entity;
+
+import java.util.Date;
+
+public class Users {
+    private Integer id;
+
+    private String name;
+
+    private String sex;
+
+    private String password;
+
+    private String email;
+
+    private Date createAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+}
